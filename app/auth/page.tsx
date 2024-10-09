532d4aba254f7f2b963fe36bc67f87ba16b0f26f
John Doe <johndoe@email.com>
2024-10-09 23:01:26 +0200

    2

---
+++ app/auth/page.tsx
@@ -0,0 +1,14 @@
+export default function Auth() {
+    return (
+        <div className="relative h-full w-full bg-[url('./images/home-background.png')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img className="w-24" src="./images/logo.jpg"  />
+
+                </nav>
+
+            </div>
+        
+        </div>
+    );
+    }
